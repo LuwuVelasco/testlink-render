32714073949f3f341d7fafd9d392a050586ff832
Luciana Velasco <luwuvp22@gmail.com>
2025-10-03 14:50:45 -0400

    Cambio de nombre carpeta install, añadido config_db.inc.php

---
+++ config_db.inc.php
@@ -0,0 +1,9 @@
+<?php
+// Conexión a PostgreSQL en Render
+define('DB_TYPE','pgsql');
+define('DB_HOST','dpg-d3g0q9re5dus73ahiavg-a:5432'); // solo host:puerto, sin "postgresql://"
+define('DB_NAME','testlink_db');
+define('DB_USER','testlink_db_user');
+define('DB_PASS','DeNQsKmanqf3LSbzo4TwO40wy0UbWyHf');
+define('DB_TABLE_PREFIX',''); // usualmente vacío
+?>
