d48acefd4f3ce1964a0b4a1812d02b9224e70b2c
Luciana Velasco <luwuvp22@gmail.com>
2025-11-01 03:25:34 -0400

    Ajustes para la base de datos

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 : "${PORT:=10000}"
 sed -ri "s/Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
 sed -ri "s/<VirtualHost \*:80>/<VirtualHost \*:${PORT}>/" /etc/apache2/sites-available/000-default.conf
+export PGOPTIONS="endpoint=ep-silent-sun-afd0euia"
+export PGSSLMODE="require"
 exec apache2-foreground
